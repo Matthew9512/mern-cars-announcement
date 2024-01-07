38f9d7c6ac395b38c7b5f6c2291616249fab3298
Matthew9512 <mateuszmaciak95@gmail.com>
2024-01-07 19:02:41 +0100

    socket ev moved to server

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,54 +1,55 @@
-const io = require('socket.io');
-const usersModel = require('../models/usersModel');
+// const io = require('socket.io');
+// const usersModel = require('../models/usersModel');
 
-const socketServer = new io.Server(8800, {
-   cors: {
-      origin: 'https://justcars.vercel.app',
-   },
-});
+// const socketServer = new io.Server(8800, {
+//    cors: {
+//       // origin: 'http://127.0.0.1:5173',
+//       origin: 'https://justcars.vercel.app',
+//    },
+// });
 
-let users = [];
+// let users = [];
 
-const addUser = (userId, socketId) => {
-   !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
-};
+// const addUser = (userId, socketId) => {
+//    !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
+// };
 
-const removeUser = (socketId) => {
-   users = users.filter((user) => user.socketId !== socketId);
-};
+// const removeUser = (socketId) => {
+//    users = users.filter((user) => user.socketId !== socketId);
+// };
 
-const getUser = (userId) => {
-   return users.find((user) => user.userId === userId);
-};
+// const getUser = (userId) => {
+//    return users.find((user) => user.userId === userId);
+// };
 
-socketServer.on('connection', (socket) => {
-   //take userId and socketId from user
-   socket.on('addUser', (userId) => {
-      addUser(userId, socket.id);
-      socketServer.emit('getUsers', users);
-   });
+// socketServer.on('connection', (socket) => {
+//    //take userId and socketId from user
+//    socket.on('addUser', (userId) => {
+//       addUser(userId, socket.id);
+//       socketServer.emit('getUsers', users);
+//    });
 
-   // send typing message
-   socket.on('typing', ({ reciverId, senderId, username }) => {
-      const user = getUser(reciverId);
-      socketServer.to(user?.socketId).emit('isTyping', { username, senderId });
-   });
+//    // send typing message
+//    socket.on('typing', ({ reciverId, senderId, username }) => {
+//       const user = getUser(reciverId);
+//       socketServer.to(user?.socketId).emit('isTyping', { username, senderId });
+//    });
 
-   socket.on('sendMessage', async ({ senderId, reciverId, message }) => {
-      const user = getUser(reciverId);
+//    socket.on('sendMessage', async ({ senderId, reciverId, message }) => {
+//       const user = getUser(reciverId);
 
-      socketServer.to(user?.socketId).emit('getMessage', {
-         senderId,
-         message,
-         reciverId,
-      });
-   });
+//       socketServer.to(user?.socketId).emit('getMessage', {
+//          senderId,
+//          message,
+//          reciverId,
+//       });
+//    });
 
-   //when disconnect
-   socket.on('disconnect', () => {
-      removeUser(socket?.id);
-      socketServer.emit('getUsers', users);
-   });
-});
+//    //when disconnect
+//    socket.on('disconnect', () => {
+//       removeUser(socket?.id);
+//       socketServer.emit('getUsers', users);
+//    });
+// });
 
-module.exports = socketServer;
+// module.exports = socketServer;
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const compression = require('compression');
+const io = require('socket.io');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const corsOptions = require('./config/corsOptions');
 const connectDB = require('./config/mongoDbCon');
 const errorHandler = require('./middleware/errorHandler');
-require('./config/socket');
+// require('./config/socket');
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -29,6 +30,56 @@ app.use('/message', require('./router/chatRouter'));
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+const expressServer = app.listen(PORT, () => {
    console.log(`server is live`);
+});
+
+const socketServer = new io.Server(expressServer, {
+   cors: {
+      origin: 'https://justcars.vercel.app',
+   },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+   !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+   users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+   return users.find((user) => user.userId === userId);
+};
+
+socketServer.on('connection', (socket) => {
+   //take userId and socketId from user
+   socket.on('addUser', (userId) => {
+      addUser(userId, socket.id);
+      socketServer.emit('getUsers', users);
+   });
+
+   // send typing message
+   socket.on('typing', ({ reciverId, senderId, username }) => {
+      const user = getUser(reciverId);
+      socketServer.to(user?.socketId).emit('isTyping', { username, senderId });
+   });
+
+   socket.on('sendMessage', async ({ senderId, reciverId, message }) => {
+      const user = getUser(reciverId);
+
+      socketServer.to(user?.socketId).emit('getMessage', {
+         senderId,
+         message,
+         reciverId,
+      });
+   });
+
+   //when disconnect
+   socket.on('disconnect', () => {
+      removeUser(socket?.id);
+      socketServer.emit('getUsers', users);
+   });
 });
